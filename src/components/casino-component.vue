77c171bc91d3aa3f3959478b297747dff94da2c5
makehoney <makehoney3@gmail.com>
2018-08-07 18:37:48 +0900

    add getContract.js

---
+++ src/components/casino-component.vue
@@ -0,0 +1,23 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'casino',
+  mounted () {
+    console.log('dispatching getContractInstance')
+    this.$store.dispatch('getContractInstance')
+  },
+  data () {
+    return {
+      amount: null,
+      pending: false,
+      winEvent: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
